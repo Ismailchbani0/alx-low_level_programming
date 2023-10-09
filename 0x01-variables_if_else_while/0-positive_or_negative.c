fd1b41f533acc971302344cc10a3fb4dcc18e0fb
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-09 07:13:42 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,19 +12,26 @@
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() % 201 - 100; /* Generates a random number between -100 and 100 */
+    srand(time(0));
+    n = rand() % 201 - 100; /* Generates a random number between -100 and 100 */
 
-	printf("%d is ", n);
+    printf("%d is ", n);
 
-	if (n > 0)
-		printf("positive\n");
-	else if (n < 0)
-		printf("negative\n");
-	else
-		printf("zero\n");
+    if (n > 0)
+        printf("positive\n");
+    else if (n < 0)
+        printf("negative\n");
+    else
+        printf("zero\n");
 
-	return (0);
+    return (0);
 }
+I have corrected the issue in the code, and now it should produce the expected output for positive, negative, and zero values of n.
+
+
+
+
+
+
