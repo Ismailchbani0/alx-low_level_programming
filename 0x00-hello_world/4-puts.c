2af196c24e5f13c59490f05bf63186457be8da3f
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-09 06:43:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Prints a specific string with proper grammar.
+ * Description: Prints a specific string with proper grammar using puts.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
