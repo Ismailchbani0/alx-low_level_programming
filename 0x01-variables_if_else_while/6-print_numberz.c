dd3e18a547ceb95cda57288466917e3401ac2287
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-09 13:19:10 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10 followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int number = 0;
+
+    while (number <= 9)
+    {
+        putchar(number + '0'); /* Convert the number to its character representation */
+        number++;
+    }
+
+    putchar('\n'); /* Print a new line */
+
+    return (0);
+}
