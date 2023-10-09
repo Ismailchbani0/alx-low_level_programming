cf7c759409d76233a27ba5077b16c3f7182421a8
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-09 07:07:33 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@ int main(void)
     int n;
 
     srand(time(0));
-    n = rand() % 201 - 100; // Generates a random number between -100 and 100
+    n = rand() % 201 - 100; /* Generates a random number between -100 and 100 */
 
     printf("%d is ", n);
 
