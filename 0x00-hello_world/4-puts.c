6b36c914458ca3517de8a74797e8f20ec3fc43e0
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-09 05:51:19 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints a message and returns 0.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+    puts("Programming is like building a multilingual puzzle\n");
+    return (0);
 }
