b7cd91770aace9086ea1c252178bd80acc31a83b
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 14:55:16 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,10 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower(int c)
-{
-    if (c >= 'a' && c <= 'z')
-        return (1);
-    else
-        return (0);
-}
+	int _islower(int c)
+	{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+	}
