2ca509a44b209a2f2b86093f32ee9dee3ced9a01
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 07:23:58 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-
+/**
+ * main - Entry point for the program
+ *
+ * This function prints the string "_putchar" followed by a new line
+ * to the standard output (stdout).
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("_putchar\n");
