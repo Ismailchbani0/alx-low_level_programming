4a04af3ff6767a8571f3de04d29355ee889fe941
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 06:03:15 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
 
 /* Declaration of the _putchar function */
 int _putchar(char c);
 
-#endif /* MAIN_H */
+int main(void)
+{
+    /* Call _putchar to print "_putchar" */
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
+}
+
+/* Definition of the _putchar function */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
