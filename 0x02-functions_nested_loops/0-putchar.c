9f53f51afe0b2b258a380ec9ed2a00670ecf6018
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 07:19:12 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>  // Include the <unistd.h> header for the write function
-
-/* Declaration of the _putchar function */
-int _putchar(char c);
 
 int main(void)
 {
-    /* Call _putchar to print "_putchar" */
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
+    printf("_putchar\n");
     return (0);
-}
-
-/* Definition of the _putchar function */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
 }
