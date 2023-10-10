26774c50b380898dad2f30be76a409b0aba15a55
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 14:29:04 -0700

    islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Declaration of the _putchar function */
-int _putchar(char c);
+int _islower(int c);
 
-/* Declaration of the print_alphabet_x10 function */
-void print_alphabet_x10(void);
-
-#endif /* MAIN_H */
+#endif
