ed9cc0485bfcd29d16628c92d184e79c0d7c8dbf
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-10 17:04:08 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's importan

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,14 @@
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
-	{
-	int last_digit;
+{
+    int last_digit;
 
-	if (n < 0)
-	{	
-		last_digit =(-n % 10);
-	else
-		last_digit =(n % 10);
-	
-	_putchar('0' + last_digit);
-	}
-	return last_digit;
-	}
+    if (n < 0)
+        last_digit = -n % 10;
+    else
+        last_digit = n % 10;
+
+    _putchar('0' + last_digit);
+    return last_digit;
+}
