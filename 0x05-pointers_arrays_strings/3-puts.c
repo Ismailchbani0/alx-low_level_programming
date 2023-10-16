139018dbbe038a355784916ee72147695a130839
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-16 08:30:11 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to write.
- *
- * Return: The number of characters written, which is always 1.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
  * _puts - Prints a string, followed by a new line.
  * @str: A pointer to a string.
  */
