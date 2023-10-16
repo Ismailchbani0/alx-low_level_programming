595b13f0d5a342588c9256e4b53f45c94c5ca5da
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-16 08:21:23 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
 int main(void)
 {
-    int n;
+    char *str;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
     return (0);
 }
