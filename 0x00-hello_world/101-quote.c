915702d5f91acd858eba0de4c1e7d5a437edddc0
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-22 15:25:11 -0700

    aflknv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the specified text to the standard error.
+ *              It does not use printf or puts. It returns 1.
+ * Return: Always 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
