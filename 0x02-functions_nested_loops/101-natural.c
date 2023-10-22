fad4598be853b17ca4d87c6feff36b94b39804d4
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-22 15:36:20 -0700

    eze

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for the program.
+ * Description: This program calculates and prints
+ * below 1024. It uses a loop to find and add the multiples.
+ * Return: Always 0 to indicate successful execution.
+ */
 int main(void)
 {
 	int sum = 0;
