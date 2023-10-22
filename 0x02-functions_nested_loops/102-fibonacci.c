ed2ab374f21534ba425623bf40691500a37e4b2b
ismailchbani0 <ismailchbani0@gmail.com>
2023-10-22 15:47:29 -0700

    ceze

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point for the program.
+ * Description: This program calculates and prints
+ * starting with 1 and 2, separated by a comma and space.
+ * Return: Always 0 to indicate successful execution.
+ */
 int main(void)
 {
 	int i;
